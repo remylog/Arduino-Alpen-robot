0aa0b8a1e8b6b082b1afe9337d15bfdf757450bc
レミィログ <sdu@outlook.jp>
2022-07-26 09:38:15 +0900

    更新

--- Program.ino/Program.ino.ino
+++ Program.ino/Program.ino.ino
@@ -12,6 +12,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if (digitalRead(9) == HIGH) {
+    delay(3000);
     digitalWrite(2, HIGH);
     digitalWrite(5, HIGH);
     delay(3000);
